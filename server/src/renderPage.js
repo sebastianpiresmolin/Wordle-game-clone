56fb13a21a788105fd6e710770dee19fb5b200bb
sebastianpiresmolin <piresmolinsecure@gmail.com>
2024-03-31 15:08:16 +0200

    add navbar partial

--- server/src/renderPage.js
+++ server/src/renderPage.js
@@ -1,3 +1,23 @@
 export default async function renderPage(res, page) {
-  res.render(page);
+  const currentPath = page == 'index' ? '/' : `/${page}`;
+  res.render(page, {
+    menuItems: MENU.map((item) => {
+      return {
+        active: currentPath == item.link,
+        label: item.label,
+        link: item.link,
+      };
+    }),
+  });
 }
+
+const MENU = [
+  {
+    label: 'Leaderboards',
+    link: '/leaderboards',
+  },
+  {
+    label: 'About',
+    link: '/about',
+  },
+];
